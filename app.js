527891f0ec071260172502cb1b7dc68caae929d6
Kancherla Dilip <138967561+dilip9398@users.noreply.github.com>
2025-02-09 14:56:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+    });
     $(window).on('scroll load', function () {
         if ($(window).scrollTop() > 60) {
             $('.header').addClass('active');
@@ -6,48 +10,46 @@ $(document).ready(function () {
             $('.header').removeClass('active');
         }
     });
+    $(window).on('scroll', function () {
+        $('section').each(function () {
+            let top = $(window).scrollTop();
+            let height = $(this).outerHeight();
+            let offset = $(this).offset().top - 150;
+            let id = $(this).attr('id');
+            if (top >= offset && top < offset + height) {
+                $('.navbar a').removeClass('active');
+                $(`.navbar a[href="#${id}"]`).addClass('active');
+            }
+        });
+    });
 });
 
-
-
-// counting the number animation
 function animateCount(countElement) {
     const targetNumber = +countElement.getAttribute('data-target');
     const duration = +countElement.getAttribute('data-duration');
     let currentNumber = 0;
-    // incerases the value
     const incrementStep = targetNumber / (duration / 10);
     const interval = setInterval(() => {
         currentNumber += incrementStep;
-
-        // Stops when it reaches the target
         if (currentNumber >= targetNumber) {
             currentNumber = targetNumber;
             clearInterval(interval);
         }
-
         countElement.textContent = Math.round(currentNumber) + "+";
     }, 10);
 }
 
-// when we scroll down and see the section we see the animation
 document.addEventListener("DOMContentLoaded", () => {
     const counts = document.querySelectorAll('.count');
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-
                 animateCount(entry.target);
-
-
                 observer.unobserve(entry.target);
             }
         });
     }, {
         threshold: 0.5
     });
-
     counts.forEach(count => observer.observe(count));
 });
-
-// End of counting
